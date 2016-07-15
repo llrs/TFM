e529c2e22b1f56aff82b24c7535856321b1716bf
Lluís Revilla <lluis.revilla@gmail.com>
2016-07-15 17:21:07 +0200

    Prepare for use on WGCNA

--- wgcna.R
+++ wgcna.R
@@ -1,1 +1,17 @@
 # Analyse data with WGCNA from doi:10.1136/gutjnl-2011-301146.
+
+library("GEOquery", quietly = T)
+
+# Prepare some variables
+gse_number <- "GSE28619"
+
+# Download files
+files.info <- getGEO(gse_number, destdir = "~/Documents")
+print(files.info)
+info <- files.info$GSE28619_series_matrix.txt.gz
+disease <- as.numeric(pData(info)$description) - 1
+dim(phenoData(info))
+
+data.wgcna <- t(exprs(info))
+
+save(data.wgcna, disease, file = "Input.RData")
