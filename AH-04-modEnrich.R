47423320c571dbf2731735a1ccbd521af217a429
Lluís Revilla <lluis.revilla@gmail.com>
2016-07-20 11:24:39 +0200

    Adding file for further study of interesting modules

---
+++ AH-04-modEnrich.R
@@ -0,0 +1,10 @@
+#  Analyse the modules with globaltest o GOstats, SPIA, hopach
+
+lnames <- load(file = "TNF_AH-network-auto.RData") 
+lnames
+lnames <- load(file = "Input.RData") 
+lnames
+lnames <- load(file = "ME.RData")
+lnames
+lnames <- load(file = file = "Module_info.RData")
+lnames
