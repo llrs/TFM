aaa5da80d055a954c50ae2d2289c9458b2576ac0
Lluís Revilla <lluis.revilla@gmail.com>
2016-07-25 11:40:04 +0200

    Merging some modules highly correlated using 0.25 as threshold

--- AH-02-networkConstr-auto.R
+++ AH-02-networkConstr-auto.R
@@ -30,29 +30,29 @@ load(file = "InputWGCNA.RData", verbose = TRUE)
 
 # Choose a set of soft-thresholding powers
 powers = c(1:30)
-# Call the network topology analysis function
-sft <- pickSoftThreshold(data.wgcna, powerVector = powers, verbose = 5, 
-                        networkType = "signed")
-# Plot the results:
-pdfn(file = "Power_calculations.pdf", width = 9, height = 5)
-par(mfrow = c(1, 2)) 
-cex1 <- 0.9 
-
-# Scale-free topology fit index as a function of the soft-thresholding power
-plot(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2], 
-     xlab = "Soft Threshold (power)", 
-     ylab = "Scale Free Topology Model Fit, signed R^2", type = "n", 
-     main = paste("Scale independence")) 
-text(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2], 
-     labels = powers, cex = cex1, col = "red") 
-# this line corresponds to using an R^2 cut-off of h
-abline(h = 0.90, col = "red")
-# Mean connectivity as a function of the soft-thresholding power
-plot(sft$fitIndices[, 1], sft$fitIndices[, 5], 
-     xlab = "Soft Threshold (power)", ylab = "Mean Connectivity", type = "n", 
-     main = paste("Mean connectivity"))
-text(sft$fitIndices[, 1], sft$fitIndices[, 5], labels = powers, cex = cex1, 
-     col = "red")
+# # Call the network topology analysis function
+# sft <- pickSoftThreshold(data.wgcna, powerVector = powers, verbose = 5, 
+#                         networkType = "signed")
+# # Plot the results:
+# pdfn(file = "Power_calculations.pdf", width = 9, height = 5)
+# par(mfrow = c(1, 2)) 
+# cex1 <- 0.9 
+# 
+# # Scale-free topology fit index as a function of the soft-thresholding power
+# plot(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2], 
+#      xlab = "Soft Threshold (power)", 
+#      ylab = "Scale Free Topology Model Fit, signed R^2", type = "n", 
+#      main = paste("Scale independence")) 
+# text(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3])*sft$fitIndices[, 2], 
+#      labels = powers, cex = cex1, col = "red") 
+# # this line corresponds to using an R^2 cut-off of h
+# abline(h = 0.90, col = "red")
+# # Mean connectivity as a function of the soft-thresholding power
+# plot(sft$fitIndices[, 1], sft$fitIndices[, 5], 
+#      xlab = "Soft Threshold (power)", ylab = "Mean Connectivity", type = "n", 
+#      main = paste("Mean connectivity"))
+# text(sft$fitIndices[, 1], sft$fitIndices[, 5], labels = powers, cex = cex1, 
+#      col = "red")
 
 # ==============================================================================
 #
@@ -60,20 +60,22 @@ text(sft$fitIndices[, 1], sft$fitIndices[, 5], labels = powers, cex = cex1,
 #
 # ==============================================================================
 
-print(paste("Recomended power", sft$powerEstimate))
-net <- blockwiseModules(data.wgcna, power = sft$powerEstimate, 
-                       TOMType = "signed", minModuleSize = 30, 
-                       maxBlockSize = 8000, networkType = "signed", 
-                       pamRespectsDendro = FALSE, 
-                       saveTOMs = TRUE, 
-                       saveTOMFileBase = "AH_signed", 
-                       verbose = 3)
+# print(paste("Recomended power", sft$powerEstimate))
+# net <- blockwiseModules(data.wgcna, power = 12, # the max connectivity of 0.77
+#                 # Power SFT.R.sq   slope truncated.R.sq mean.k. median.k. max.k.
+#                 # 12     0.7730  -1.490          0.738     602     400.0   2220
+#                        TOMType = "signed", minModuleSize = 30, 
+#                        maxBlockSize = 8000, networkType = "signed", 
+#                        pamRespectsDendro = FALSE, 
+#                        saveTOMs = TRUE, 
+#                        saveTOMFileBase = "AH_signed", 
+#                        verbose = 3)
 
-save(net, file = "net.RData")
-# load("net.RData")
+# save(net, file = "net.RData")
+load("net.RData")
 # ==============================================================================
 #
-#  Code chunk 4: Plot how the modules correlate in a dendrogram
+#  Code chunk 4: Plot how the modules correlate in a first dendrogram
 #
 # ==============================================================================
 
@@ -90,18 +92,37 @@ plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
 
 # ==============================================================================
 #
-#  Code chunk 5: Save data for the next step
+#  Code chunk 5: See how are the modules found
 #
 # ==============================================================================
 
+# Calculate eigengenes
+MEList <- moduleEigengenes(data.wgcna, colors = net$colors)
+MEs <- MEList$eigengenes
+# Calculate dissimilarity of module eigengenes
+corME <- cor(MEs)
+MEDiss <- 1 - corME
+# Cluster module eigengenes
+METree <- hclust(as.dist(MEDiss), method = "average")
+# Plot the result
+pdfn("Modules_relationship.pdf")
+plot(METree, main = "Clustering of module eigengenes",
+     xlab = "", sub = "")
+MEDissThres <- 0.25
+# Plot the cut line into the dendrogram
+abline(h = MEDissThres, col = "red")
+
+labeledHeatmap(corME, xLabels = colnames(corME), yLabels = colnames(corME),
+               sSymbols = colnames(corME), ySymbols = colnames(corME))
+
 gm <- table(net$colors)
+gm
 hist(gm)
 count.p <- function(data, per){
   # Calculate the percentatge above per of data
   sum(data >= per)/length(data)
 }
 perc <- unlist(lapply(gm, count.p, data = gm))
-pdfn(file = "modules_distr.pdf")
 plot(cbind(gm[order(perc)],perc[order(perc)]), type = "o",  
      xlab = "Size of the modules", 
      ylab = "Proportion of modules above the size",
@@ -110,8 +131,23 @@ plot(cbind(gm[order(perc)],perc[order(perc)]), type = "o",
 
 
 dev.off()
-moduleColors = net$colors
-MEs = net$MEs 
-geneTree = net$dendrograms[[1]] 
-dendro <- net$dendrograms
+
+
+# Call an automatic merging function
+merge <- mergeCloseModules(data.wgcna, 
+                          net$colors, cutHeight = MEDissThres, MEs = MEs,
+                          verbose = 3)
+# The merged module colors
+mergedColors <- merge$colors;
+# Eigengenes of the new merged modules:
+mergedMEs <- merge$newME
+
+# ==============================================================================
+#
+#  Code chunk 6: Save the data 
+#
+# ==============================================================================
+
+moduleColors <- mergedColors
+MEs <- mergedMEs
 save(MEs, moduleColors, file = "TNF_AH-network-auto.RData")
