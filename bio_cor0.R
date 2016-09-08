3c207e2e5a1ac7845214c021e7dece47acd309a6
Lluís Revilla <lluis.revilla@gmail.com>
2016-09-08 15:15:29 +0200

    Explain a step of bio.cor

--- bio_cor0.R
+++ bio_cor0.R
@@ -9,7 +9,7 @@ library("KEGG.db")
 library("RBGL")
 
 ".combinadic" <- function(n, r, i) {
-  
+
   # http://msdn.microsoft.com/en-us/library/aa289166(VS.71).aspx
   # http://en.wikipedia.org/wiki/Combinadic
   n0 <- length(n)
@@ -21,7 +21,7 @@ library("RBGL")
     while (choose(v,r) >= i) v <- v - 1        # Adjusted for one-based indexing
     return(v)
   }
-  
+
   res <- rep(NA,r)
   for (j in 1:r) {
     res[j] <- largestV(n0,r,i)
@@ -51,7 +51,7 @@ go_cor <- function(e_a, e_b, chip = "hgu133plus2.db", ...){
   # https://support.bioconductor.org/p/85702/#85732
   LP <- simLL(e_a, e_b, "BP", measure = "LP", chip = chip, ...)
   UI <- simLL(e_a, e_b, "BP", measure = "UI", chip = chip, ...)
-  
+
   if (is.na(LP) | is.na(UI)) {
     return(NA)
   }
@@ -64,32 +64,36 @@ go_cor <- function(e_a, e_b, chip = "hgu133plus2.db", ...){
     plens <- subListExtract(paths, "length", simplify = TRUE)
     max(plens)
   }
-  # Calculates the score taking into account the size and the middnest path
+  # Calculates the score taking into account the size and the middle path
   # Taking advantage of the fact that in GO there is a root and leaves
+  # UI: Union intersect, is the size of the intersection of the node
+  #        sets divided by the size of the union of the node sets
+  # LP: longest path, is the longest path in the intersection graph of
+  #                the two supplied graph.
   (UI$sim/LP$sim)*max(s.path(LP$g1), s.path(LP$g2))
 }
 
 kegg_cor <- function(react_a, react_b){
   # Function that correlates based on reactome ids
   # Basically calculates how many nodes do overlap between pathways
-  
+
   if (is.na(react_a) | is.na(react_b)) {
     return(NA)
   } else if (react_a == react_b) {
     return(1)
   }
-  
+
   # Retrieve the results from internet https site
   tmp <- tempfile("hsa")
-  retrieveKGML(paste0("path:", react_a), "hsa", tmp, method = "wget", 
+  retrieveKGML(paste0("path:", react_a), "hsa", tmp, method = "wget",
                quiet = TRUE)
   g1 <- parseKGML2Graph(tmp, expandGenes = TRUE)
-  
+
   tmp2 <- tempfile("hsa")
-  retrieveKGML(paste0("path:", react_b), "hsa", tmp2, method = "wget", 
+  retrieveKGML(paste0("path:", react_b), "hsa", tmp2, method = "wget",
                quiet = TRUE)
   g2 <- parseKGML2Graph(tmp2, expandGenes = TRUE)
-  
+
   score <- compare_graphs(g1, g2)
   score
 }
@@ -108,7 +112,7 @@ dist_cor <- function(a, b, info){
     score <- 0
   }
   score <- 0.5 # If in the same chromosome at least 0.5 distance correlation
-  
+
   # Maybe the default for the same chromosome could be substituted by the
   # CM distance
   # 5*10^5 is the region of upstream/downstream where regulation usually occurs
@@ -119,13 +123,13 @@ dist_cor <- function(a, b, info){
     start_dist <- info_a["start_position"] - info_a["end_position"]
     end_dist <-  info_b["start_position"] - info_b["end_position"]
   }
-  
+
   if (abs(start_dist) < 5*10 ^ 5) {
     score <- score + 0.25
   } else if (abs(end_dist) < 5*10 ^ 5) {
     score <- score + 0.25
   }
-  
+
   # Using the cathegory of biotypes to score them
   biotypes <- c("miRNA", "snRNA", "snoRNA", "scaRNA", "lincRNA", "lncRNA")
   if (((info_a["gene_biotype"] %in% biotypes)  &
@@ -144,25 +148,25 @@ dist_cor <- function(a, b, info){
 react_cor <- function(react_a, react_b, hR){
   # Function that correlates based on reactome ids
   # Basically calculates how many nodes do overlap between pathways
-  
+
   if (is.na(react_a) | is.na(react_b)) {
     return(NA)
   } else if (react_a == react_b) {
     return(1)
   }
-  
+
   ids <- sapply(hR, function(x){x@id})
   react_name <- names(hR[ids %in% react_a])
   react_name2 <- names(hR[ids %in% react_b])
-  
+
   if (length(react_name) != 1 | length(react_name2) != 1) {
     return(NA)
   }
-  
+
   # Obtain each pathway
   g1 <- hR[[react_name]]
   g2 <- hR[[react_name2]]
-  
+
   score <- compare_graphs(g1, g2)
   score
 }
@@ -176,7 +180,7 @@ comb2mat <- function(input, func, ...){
     cobs[[i]] <- .combinadic(input, 2, i)
   }
   # cobs <- combn(input, 2)
-  
+
   N <- sapply(cobs, function(x, ...){func(x[1], x[2], ...)})
   # Function that performs the calculus
   # N <- seq_len(ncol(combs))
@@ -206,7 +210,7 @@ comb_biopath <- function(comb, info, by, biopath){
   # react_path <- apply(comb, 2, function(y){
   a <- unique(info[info[by] == comb[1], biopath])
   a <- a[a != ""]
-  
+
   b <- unique(info[info[by] == comb[2], biopath])
   b <- b[b != ""]
   if (all(sapply(a, is.na)) | all(sapply(b, is.na))) {
@@ -236,7 +240,7 @@ bio.cor <- function(x, ... ){
   names_probes <- x
   humanReactome <- pathways("hsapiens", "reactome")
   # humanKegg <- pathways("hsapiens", "kegg")
-  
+
   mart <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
   attri <- c("affy_hg_u133_plus_2","entrezgene",
              "gene_biotype", "start_position", "end_position", "chromosome_name",
@@ -245,13 +249,13 @@ bio.cor <- function(x, ... ){
   info <- getBM(attributes = attri,
                 filters = "affy_hg_u133_plus_2", values = names_probes,
                 mart = mart)
-  
+
   affy.id <- select(hgu133plus2.db, keys = names_probes,
                     columns = c("PROBEID", "ENTREZID", "SYMBOL", "PATH"))
-  
+
   # go_mat <- comb2mat(names_probes, go_cor)
   # dist_mat <- comb2mat(x, dist_cor, info) # Not useful
-  
+
   # TODO: extract a lab notebook
   # comb <- combn(names_probes, 2)
   react.bio <- rep(NA, choose(length(names_probes), 2))
@@ -290,7 +294,7 @@ bio.cor <- function(x, ... ){
         result <- tryCatch({
           kegg_cor(y[1], y[2])
           }, warning = function(w) {
-            message(paste("Warning downloading the data for ", 
+            message(paste("Warning downloading the data for ",
                           y[1], "or", y[2]))
             message(w)
           }, error = function(e) {
@@ -301,19 +305,19 @@ bio.cor <- function(x, ... ){
         }
       )
     }
-    
+
     if (length(a) != 0) {
       kegg.bio[i] <- max(a, na.rm = TRUE)
     } else {
       kegg.bio[i] <- NA
     }
     # }})
-    
+
   }
   react_mat <- seq2mat(names_probes, react.bio)
   kegg_mat <- seq2mat(names_probes, kegg.bio)
   cor_mat <- list(reactome = react_mat, kegg = kegg_mat, go = go_mat)
-  
+
 }
 
 weighted <- function(x, w){
