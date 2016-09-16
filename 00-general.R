83e4a021e0552874d939b775d58df501fdb83251
Lluís Revilla <lluis.revilla@gmail.com>
2016-09-16 12:46:00 +0200

    Adding functions to trim blank spaces

--- 00-general.R
+++ 00-general.R
@@ -339,5 +339,19 @@ level.na <- function(vec){
   vec
 }
 
+# returns string w/o leading whitespace
+trim.leading <- function(x) {
+  sub("^\\s+", "", x)
+}
+
+# returns string w/o trailing whitespace
+trim.trailing <- function(x) {
+  sub("\\s+$", "", x)
+}
+
+# returns string w/o leading or trailing whitespace
+trim <- function(x) {
+  gsub("^\\s+|\\s+$", "", x)
+}
 
 
