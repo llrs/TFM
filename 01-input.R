84d5d315e0edce0cd207d804ab02996c4f9cc4c0
Lluís Revilla <lluis.revilla@gmail.com>
2016-09-28 17:52:00 +0200

    Preparing the clinical data for merging them

--- 01-input.R
+++ 01-input.R
@@ -1,10 +1,13 @@
 # Read the files
 
-source("00-general.R")
+source("/home/lrevilla/Documents/TFM/00-general.R", echo = TRUE)
 
 pheno <- read.csv("miRNA_phenotype.csv")
 exprs <- read.csv("miRNA_normQ.csv", row.names = 1)
-vclin <- read.csv("DB_MIR-Alcoholic_Hepatitis_Sept2015.csv")
+exprs2 <- read.table("normalised_ASH_mirna.csv", sep = "\t")
+vclin <- read.csv(file.path("..", "DB_MIR-Alcoholic_Hepatitis_Sept2015.csv"))
+vclin2 <- read.csv(file.path("..", "DB_integrated.CCL20-Alcoholic_Hepatitis_19_06_2013_(2)_1_.csv"))
+setwd(data.files.out)
 # ==============================================================================
 #
 #  Code chunk 1: Preparing the clinical data
@@ -18,21 +21,15 @@ AH <- paste0("HA-", c(23, 24, 30, 36, 39, 51, 54, 57, 60, 77, 91, 84, 10, 90,
 normal <- paste0("NQ-", c(2:5))
 ids <- c(AH, normal)
 samplename <- substring(pheno$sampleid, 8)
-convert <- function(x){
-  #Remove the 0 of the second position
-  ifelse(substring(x, 2, 2) == "0", paste0(substring(x, 1, 1),
-                                           substring(x, 3, 3)),
-         x)
-}
+
 samplename <- convert(samplename)
-pheno <- cbind(pheno, "samplename" = samplename,
+pheno2 <- cbind(pheno, "samplename" = samplename,
                "group" = as.factor(substring(pheno$GROUP, 1, 2)),
                "patientid" = ids)
 
-vclin <- merge(pheno, vclin, by.x = "patientid", by.y = "id", all.x = TRUE)
-inter_v <- c("patientid", "sampleid", "samplename", "group",
-             grep("mir_", colnames(vclin), fixed = T, value = T), "age",
-             "gender", "meld", "lille", "glucose", "follow_up_90", "status_180",
+vclin <- merge(pheno2, vclin, by.x = "patientid", by.y = "id", all.x = TRUE)
+inter_v <- c("patientid", "sampleid", "samplename", "group", "age",
+             "gender", "meld", "lille", "glucose", "status_180",
              "ast", "alt")
 am <- c("status_90", "infection_hospitalization", "aki", "hvpg_corte20",
         "hvpg", "maddrey", "status_1year", "creatinine", "ap", "ggt",
@@ -40,7 +37,17 @@ am <- c("status_90", "infection_hospitalization", "aki", "hvpg_corte20",
 vars <- c(am, inter_v)
 pheno <- vclin[, colnames(vclin) %in% vars]
 
+pheno$status_90 <- ifelse(pheno$status_90 == "alive", 1, 0)
+pheno$status_180 <- ifelse(pheno$status_180 == "alive", 1, 0)
+pheno$status_1year <- ifelse(pheno$status_1year == "alive", 1, 0)
+pheno$gender <- fact2num(pheno$gender, "Male", 1)
+pheno$gender <- fact2num(pheno$gender, "Female", 0)
 
+ids_2 <- sub("_\\..*", "", colnames(exprs2))
+ids_2 <- sub("\\.", "", ids_2)
+vclin2 <- vclin2[vclin2$id %in% ids_2, colnames(vclin2) %in% vars]
+
+v <- rbind.fill(pheno, vclin2)
 # ==============================================================================
 #
 #  Code chunk 2: Preparing the expression data
